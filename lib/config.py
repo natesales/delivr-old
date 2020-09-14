b95ffa6877a78942d65e8f0990463312d36d7d8f
Nate Sales <nate@natesales.net>
2020-09-13 20:48:16 -0700

    Add route family parser

--- lib/config.py
+++ lib/config.py
@@ -2,21 +2,20 @@ import os
 
 import yaml
 
-# All configuration values/types
-configuration = {
-    "asn": 0,
-    "ipv4_routes": [],
-    "ipv6_routes": [],
-    "database": "",
-    "salt": "",
-    "server_host": "",
-    "server_port": 0,
-    "nameservers": []
-}
-
 if not os.path.exists("config.yml"):
     print("No config.yml file found.")
     exit(1)
 
 with open("config.yml", "r") as config_file:
     configuration = yaml.safe_load(config_file.read())
+
+configuration["ipv4_routes"] = ""
+configuration["ipv6_routes"] = ""
+for route in configuration["routes"]:
+    if ":" in route:
+        configuration["ipv6_routes"] += route + ","
+    else:
+        configuration["ipv4_routes"] += route + ","
+
+configuration["ipv4_routes"] = configuration["ipv4_routes"][:-1]
+configuration["ipv6_routes"] = configuration["ipv6_routes"][:-1]
