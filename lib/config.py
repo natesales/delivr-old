6d92c6d14f51a6c8dd50f8f7a51d0c018fd495bf
Nate Sales <nate@natesales.net>
2020-09-13 20:12:43 -0700

    Remove explicit values from configuration parser

--- lib/config.py
+++ lib/config.py
@@ -19,21 +19,4 @@ if not os.path.exists("config.yml"):
     exit(1)
 
 with open("config.yml", "r") as config_file:
-    _config = yaml.safe_load(config_file.read())
-
-
-# Database
-configuration["database"] = _config["database"]
-
-# Authentication
-configuration["salt"] = _config["salt"]
-
-# Server
-configuration["server_host"] = _config["server-host"]
-configuration["server_port"] = _config["server-port"]
-
-configuration["development"] = _config["development"]
-
-# Site Specific
-configuration["nameservers"] = _config["nameservers"]
-configuration["soa_root"] = _config["soa-root"]
+    configuration = yaml.safe_load(config_file.read())
